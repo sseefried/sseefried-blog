cbd6a6d9706425a188622ce7e16732745493f745
Sean Seefried <sean@seefried.com.au>
2022-01-06 16:27:57 +1000

    Add link to Stack Overlow in comment

--- fix-links.lua
+++ fix-links.lua
@@ -1,3 +1,4 @@
+-- https://stackoverflow.com/questions/48169995/pandoc-how-to-link-to-a-section-in-another-markdown-file
 function Link (link)
   link.target = link.target:gsub('(.+)%.md', '%1.html')
   return link
