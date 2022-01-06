1eb33a3ee1fea40cbd37eb12ddd8d15a6afc8fda
Sean Seefried <sean@seefried.com.au>
2022-01-06 16:26:45 +1000

    Add index.html

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd src
 
 for src in *.md; do
   OUT=$(basename $src .md).html
-  pandoc --toc -s -c css/default.css $src -o ../site/$OUT
+  pandoc --lua-filter ../fix-links.lua -s -c css/default.css $src -o ../site/$OUT
 done
---
+++ src/index.md
@@ -0,0 +1,3 @@
+% Sean Seefried's blog
+
+- [Introducing a series of posts on the semantics of C](./intro-to-no-semantics-of-c.md)
