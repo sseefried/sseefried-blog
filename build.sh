1f5a54ada2524eb0ed58a05c5e85cdf0c7238c3c
Sean Seefried <sean@seefried.com.au>
2022-01-07 16:14:44 +1000

    Add new draft and change build script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-cd src
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $BASE/src
+
+mkdir -p $BASE/site
 for src in *.md; do
   OUT=$(basename $src .md).html
-  pandoc --lua-filter ../fix-links.lua -s -c css/default.css $src -o ../site/$OUT
+  pandoc --lua-filter $BASE/fix-links.lua -s -c css/default.css $src -o $BASE/site/$OUT
+done
+
+cd $BASE/src/drafts
+
+mkdir -p $BASE/site/drafts
+for src in *.md; do
+  OUT=$(basename $src .md).html
+  pandoc --lua-filter $BASE/fix-links.lua -s -c css/default.css $src -o $BASE/site/drafts/$OUT
 done
