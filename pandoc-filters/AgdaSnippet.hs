14b7bdb90364feceb4c3366b255cf9cc59ce964b
Sean Seefried <sean@seefried.com.au>
2022-02-23 14:36:51 +1000

    Add minimal filter

---
+++ pandoc-filters/AgdaSnippet.hs
@@ -0,0 +1,19 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE OverloadedStrings #-}
+
+-- includes.hs
+import Text.Pandoc.JSON
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+
+doInclude :: Block -> IO Block
+doInclude cb@(CodeBlock (id, classes, namevals) contents) =
+  case lookup "include" namevals of
+       Just f     -> CodeBlock (id, classes, namevals) <$>
+                      TIO.readFile (T.unpack f)
+       Nothing    -> return cb
+doInclude x = return x
+
+main :: IO ()
+main = toJSONFilter doInclude
