6cc92eac69d7e74ef2dbb43e08eca89b6c341a4d
Sean Seefried <sean@seefried.com.au>
2022-02-24 14:23:22 +1000

    Complete pandoc filter

--- pandoc-filters/AgdaSnippet.hs
+++ pandoc-filters/AgdaSnippet.hs
@@ -3,16 +3,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- includes.hs
+import Data.List
 import Text.Pandoc.JSON
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
+import System.Process
+import System.Posix.Directory
 
 doInclude :: Block -> IO Block
-doInclude cb@(CodeBlock (id, classes, namevals) contents) =
-  case lookup "include" namevals of
-       Just f     -> CodeBlock (id, classes, namevals) <$>
-                      TIO.readFile (T.unpack f)
-       Nothing    -> return cb
+doInclude cb@(CodeBlock (id, classes, namevals) contents) = do
+  case ( lookup "htmlDir" namevals
+       , lookup "module" namevals
+       , lookup "def" namevals) of
+    (Just htmlSource, Just modul, Just def) -> do
+          html <- (T.pack <$>
+                    readProcess "../../pandoc-filters/mk-snippet-html.sh"
+                    (map T.unpack [ htmlSource, modul, def ]) "")
+          return $ Plain [ RawInline (Format "html")
+                      ("<pre class=\"Agda\">" <> html <> "</pre>") ]
+    _          -> do
+         return cb
 doInclude x = return x
 
 main :: IO ()
