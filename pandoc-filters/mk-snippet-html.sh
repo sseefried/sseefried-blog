3e12a817b35ec77da655d6bb50a8a73a845f123e
Sean Seefried <sean@seefried.com.au>
2022-02-24 14:22:27 +1000

    Add dodgy script for making a HTML snippet

---
+++ pandoc-filters/mk-snippet-html.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Dodgy script that will get replaced
+#
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SITE_DIR="$( cd $THIS_DIR/../site && pwd)"
+cd $THIS_DIR
+
+[ $# -lt 3 ] && { echo "Usage: $(basename $0) <basedir> <module> <id>"; exit 1;}
+
+BASE=$(cd $SITE_DIR/agda-html/$1 && pwd)
+MODULE=$2
+ID=$3
+
+#
+# FIXME: Handle drafts properly
+#
+BASE_RELATIVE=$(realpath --relative-to=$SITE_DIR/drafts $BASE | sed 's/\//\\\//g' | sed 's/\./\\./g')
+
+cd "$BASE"
+cat "$MODULE.html" | sed -n "/--pandoc-begin $ID/,/--pandoc-end $ID/p" \
+  | sed "s/href=\"\([^\"]*\)\"/href=\"$BASE_RELATIVE\/\\1\"/g" | grep -v '\-\-pandoc'
