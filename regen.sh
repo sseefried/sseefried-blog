e2080155a667ea60c928ca802f52ca355e4a2cf3
Sean Seefried <sean@seefried.com.au>
2022-02-26 13:14:35 +1000

    Update regen.sh script

--- regen.sh
+++ regen.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+ABS_AGDA_SRC="$THIS_DIR/agda-src"
+ABS_AGDA_HTML="$THIS_DIR/site/agda-html"
 
-cd agda-src/2022-02-24-permutations
-agda --html Permutations.agda --html-dir=$THIS_DIR/site/agda-html/2022-02-24-permutations
+cd $ABS_AGDA_SRC
+for relsrcdir in *; do
+  if [ -d "$relsrcdir" ]; then
+    cd $relsrcdir
+    agda --html Permutations.agda --html-dir="$ABS_AGDA_HTML/$relsrcdir"
+    cd $ABS_AGDA_SRC
+  fi
+done
