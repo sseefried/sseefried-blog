f10c18038f7896e218212cdfeb1b9a5ce5892f0a
Sean Seefried <sean@seefried.com.au>
2022-03-19 15:41:46 +1000

    Add ability to filter which posts you build using a pattern

--- build.sh
+++ build.sh
@@ -2,9 +2,21 @@
 
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+#
+# If you supply an argument then only posts mattch the PATTERN will be built
+#
+
+PATTERN=""
+if [ $# -ge 1 ]; then
+  PATTERN=$1
+fi
+
 cd $BASE/src
 for src in *.md drafts/*.md devlog/*.md; do
   # Exceptions for comments system
+  echo "$src" | grep -q "$PATTERN"
+  VAL=${PIPESTATUS[1]}
+  if [ $VAL -ne 0 ]; then continue; fi
   if [ "$src" != "index.md" -a "$src" != "devlog/index.md" ]; then
     TEMPLATE="--template=$BASE/pandoc-data/templates/post.html"
   else
